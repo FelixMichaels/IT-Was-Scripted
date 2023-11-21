85335a4f99b96256c7c0003ada1a19300f21d5fc
FelixMichaels <50510729+FelixMichaels@users.noreply.github.com>
2023-11-21 00:46:15 -0800

    Update Kandji_Uptime_Notifier.sh

--- Mac Scripts/Kandji/Kandji_Uptime_Notifier.sh
+++ Mac Scripts/Kandji/Kandji_Uptime_Notifier.sh
@@ -13,24 +13,35 @@
 #########################################################################################################
 # Script version
 VERSION="1.0"
-############################################### Variables ###############################################
+###################################### Variables ########################################################
 
 days_up=$(uptime | awk '{print $3}')
-message="Your device has been up for ${days_up} days, please restart to avoid any issues!"
-Title="Uptime Notifier"
+stat=$(uptime | awk '{print $4}')
+message="Your Device has been up for ${days_up} day(s) please restart to avoid any issues"
+Title="Uptime"
 uptime=$(uptime)
+days="4"
 
-# Kandji output for a quick view, if you want to troubleshoot issues, because restarts fix alot
+# Kandji output for a quick view
+echo "Current uptime is:" ${uptime}
 
-echo ${uptime}
+# Skips any device online less than a day"
+if ! [[ "${days_up}" =~ ^[0-9]+$ ]]
+    then
+        echo "device has been online for less than a day"
+fi
+
+if [ "${stat}" = "mins," ]
+then
+    echo "device has been online for less than a day"
+    exit 0
+fi
 
 # Alerts the user if device uptime is over specified date "Adjust the item -0, to + or - a specific #"
-# If device is online less than 1 day will pull an error in logs, but this only applies to -0 time specified
+# If device is online less than 1 day will pull an error in logs, but this only applies to -0 time specified​
 
-if [ "${days_up}" -gt "$(($(/bin/date +%D)+14))" ]
+if [ "${days_up}" -gt "${days}" ]
 then
     /usr/local/bin/kandji display-alert --title "$Title" --message "$message"
-else
-    echo "Device online less than defined amount"
-fi > dev/null
+fi > /dev/null
 exit 0
